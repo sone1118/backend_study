89381026cbf84a62c090b65df11fb0bb5a936860
Jenos <sone1118@naver.com>
2022-11-06 22:15:04 +0900

    - Schema 생성시  오류 수정     #new mongoose.Schema({}); - mongoose의 connect 옵션 수정

--- webtube/src/models/Video.js
+++ webtube/src/models/Video.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 // title: String == title: {type: String}
-const videoSchema = {
+const videoSchema = new mongoose.Schema({
     title: {type: String, trim: true, required: true, maxlength: 40},
     description: {type: String, trim: true, required: true, maxlength: 140},
     createAt: {type: Date, default: Date.now, required: true},
@@ -10,7 +10,7 @@ const videoSchema = {
         views: {type: Number, default: 0, required: true},
         rating: {type: Number, default: 0, required: true},
     }
-};
+});
 
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
